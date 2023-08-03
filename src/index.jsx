775f68755238ead805fd824e7307d80b2c3093b4
Sergio del Pino C <sdelpinocampos@hotmail.com>
2023-08-03 11:00:13 -0400

    Fix lint errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
+
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+createRoot(document.getElementById('app')).render(
+  <App />
+)
